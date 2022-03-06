fe224fc599310809cb09f7e27605fe0c92e86612
anomius <swarajpurohit@gmail.com>
2022-03-07 02:40:29 +0530

    Time: 50 ms (60.96%), Space: 14 MB (76.03%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
@@ -1,11 +1,20 @@
 class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
-        pep=[0]*num_people
-        g=1
-        while candies>g: 
-            pep[g%num_people-1]+=g
-            candies-=g
-            g+=1
-        pep[g%num_people-1]+=candies
-        return pep
-            
+        n = int(((1 + 8 * candies) ** (1 / 2) - 1) / 2)
+        rounds = n // num_people
+        last_round = n % num_people
+
+        s = [0] * num_people
+        s[last_round] = candies - ((n + 1) * n) // 2
+        cur_handout = rounds * num_people + 1
+        for i in range(last_round):
+            s[i] = cur_handout
+            cur_handout += 1
+
+        if rounds > 0:
+            cur_handout = (2 + (rounds - 1) * num_people) * rounds // 2
+            for i in range(num_people):
+                s[i] += cur_handout
+                cur_handout += rounds
+
+        return s
