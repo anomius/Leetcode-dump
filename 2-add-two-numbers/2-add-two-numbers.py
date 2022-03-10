77ac55489f02573e3e05e55fb463a02ec462c6cc
anomius <swarajpurohit@gmail.com>
2022-03-10 11:55:06 +0530

    Time: 94 ms (54.29%), Space: 13.9 MB (65.79%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,14 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        res = ListNode()
-        curr = res
-        num = 0
-        while l1 or l2 or num > 0:
-            num += (l1.val if l1 else 0) + (l2.val if l2 else 0)
-            curr.next = ListNode(num % 10)
-            num //= 10 # floor division
-            curr = curr.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return res.next
+        itr1= l1
+        itr2= l2
+        cr=0
+        k=t=ListNode(None)
+        while itr1 and itr2:
+            t.next=ListNode((itr1.val+itr2.val+cr)%10)
+            cr=(itr1.val+itr2.val+cr)//10
+            itr1=itr1.next
+            itr2=itr2.next
+            t=t.next
+        if itr1:
+            while itr1:
+                t.next=ListNode((itr1.val+cr)%10)
+                cr=(itr1.val+cr)//10
+                itr1=itr1.next
+                t=t.next
+        if itr2:
+            while itr2:
+                t.next=ListNode((itr2.val+cr)%10)
+                cr=(itr2.val+cr)//10
+                itr2=itr2.next
+                t=t.next
+        if cr!=0:
+            t.next=ListNode(cr)
+        return k.next
