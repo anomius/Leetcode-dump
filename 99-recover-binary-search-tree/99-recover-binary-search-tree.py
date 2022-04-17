f06aa6a920d82613504a7a2f559945d2ad4d40a5
anomius <swarajpurohit@gmail.com>
2022-04-17 06:19:20 +0530

    Time: 76 ms (87.92%), Space: 14.3 MB (28.86%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -9,26 +9,26 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        k=[]
-        def helper(root):
-            if root:
-                helper(root.left)
-                k.append(root.val) 
-                helper(root.right)
-        helper(root)
-        l=k.copy()
-        k.sort()
-        dic=[l[i]-k[i] for i in range(len(k))]
-       
-        global count
-        count=0
-        def helper2(root):
-            if root:
-                global count
-                helper2(root.left)
-                root.val-=dic[count] 
-                
-                count+=1
-                helper2(root.right)
-        helper2(root)
+        stack=[]
+        prev=None
+        nxt=None
+        first=None
+        middle=None
+        while stack or root:
+            while root:
+                stack.append(root)
+                root=root.left
+            tmp=stack.pop()
+            if prev!=None and prev.val>tmp.val:
+                if first==None:
+                    first=prev
+                    middle=tmp
+                else:
+                    nxt=tmp
+            prev=tmp
+            root=tmp.right
+        if first and nxt:
+            first.val,nxt.val=nxt.val,first.val
+        elif first and middle:
+            first.val,middle.val=middle.val,first.val
         
