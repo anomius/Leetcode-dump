b445ab22136f91cbbbb769e5a34bdd2f05caa9b7
anomius <swarajpurohit@gmail.com>
2022-07-17 19:19:37 +0530

    Time: 226 ms (81.36%), Space: 14.4 MB (59.09%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.py
+++ 1447-simplified-fractions/1447-simplified-fractions.py
@@ -1,11 +1,9 @@
 class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
-        l=[]
         out=[]
         for i in range(2,n+1):
             for j in range(1,i):
-                if j/i not in l:
-                    l.append(j/i)
+                if gcd(j, i) == 1:
                     out.append(f"{j}/{i}")
         return out
         
